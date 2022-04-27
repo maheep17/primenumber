04d487c3f16dc64293108e9cd86845366f1f5c49
maheep17 <104062304+maheep17@users.noreply.github.com>
2022-04-27 14:11:05 +0530

    Update primenumber.c

--- primenumber.c
+++ primenumber.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    int n,i,flag=0;
+    int n,i,flag=1;
     printf("Enter a number : ");
     scanf("%d",&n);
-    if(n==0 || n==1)
-        flag=1;
-    for(i=2;i<=n/2;++i)
+    for(i=2;i<=n;i++)
     {
         if(n%i==0)
-            flag=1;
-            break;
+            flag++;
     }
-    if(flag==0)
+    if(flag==2)
         printf("%d number is a prime number",n);
     else
         printf("%d number is not a prime number",n);
